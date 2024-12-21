a5b687ddf4f2070a098e714b97d32f89bd630f1d
dhopegraphics <dhopegraphics@gmail.com>
2024-12-21 13:57:52 +0000

    setting up project files

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="justify-center items-center bg-black ">
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
